b6f353713c6dfb6c2f19fc715dba21e58b204cdc
liruifengv <liruifeng1024@gmail.com>
2022-07-26 10:32:13 +0800

    feat: first version

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-!function(){
-  console.log('init promise modal')
-}()
+import React from 'react'
+import { render } from 'react-dom'
+import PropTypes from 'prop-types'
+
+// default Provider
+let Container = ({ children }) => <>{children}</>
+
+Container.propTypes = {
+  children: PropTypes.element.isRequired,
+}
+
+const renderModal = (Template, props) => {
+  const dom = document.createElement('div')
+  dom.setAttribute('class', 'portal-instance-container')
+  document.body.appendChild(dom)
+  const template = (
+    <Container>
+      <Template {...props} />
+    </Container>
+  )
+  render(template, dom)
+  return dom
+}
+
+export default async (template, data = {}, options = {}) => {
+  let instance = null
+  // unmount
+  const unmountedNode = () => {
+    setTimeout(() => {
+      if (instance) {
+        document.body.removeChild(instance)
+      }
+    }, options.unmountDelay || 0)
+  }
+
+  const p = new Promise((resolve, reject) => {
+    const props = {
+      callbackResolve: resolve,
+      callbackReject: reject,
+      ...data,
+    }
+    instance = renderModal(template, props)
+  })
+
+  // callbackResolve
+  const callbackResolve = val => {
+    unmountedNode()
+    return Promise.resolve(val)
+  }
+
+  // callbackReject
+  const callbackReject = err => {
+    unmountedNode()
+    return Promise.reject(err)
+  }
+
+  return p.then(callbackResolve, callbackReject)
+}
