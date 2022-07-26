d0a90e18e51025e6374708c2626158d8d8af29fc
liruifengv <liruifeng1024@gmail.com>
2022-07-26 19:57:00 +0800

    example: fix error

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <Button Button type="primary" onClick={showModal}>show modal</Button>
+      <Button type="primary" onClick={showModal}>show modal</Button>
     </div>
   )
 }
